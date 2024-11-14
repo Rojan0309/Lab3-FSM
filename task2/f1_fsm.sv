ff6086cdba373469bc13908b1c155035561338fb
Rojan Ragunathan <rojan.ragunathan@gmail.com>
2024-11-14 01:13:49 +0000

    modified:   task1/lfsr.sv 	modified:   task2/f1_fsm.sv

--- task2/f1_fsm.sv
+++ task2/f1_fsm.sv
@@ -5,4 +5,13 @@ module f1_fsm (
     output  logic [7:0] data_out
 );
 
+typedef enum logic [7:0] {S0, S1, S2, S3, S4, S5, S6, S7, S8} state;
+state current_state, next_state;
+
+always_ff @(posedge clk)
+        if (en)    current_state <= next_state;
+        else       current_state <= S0;
+
+
+
 endmodule
